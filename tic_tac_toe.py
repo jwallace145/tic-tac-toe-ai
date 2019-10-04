c40d51af4a245dd23387484b76f4941b47e51a34
James  Wallace <jameswallace@Jamess-MBP.lan1>
2019-10-04 15:34:06 -0400

    differentiated minimax methods so the player can choose depth control and alpha beta pruning etc

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,24 +4,27 @@
 # | 0,0 | 0,1 | 0,2 |
 # | 1,0 | 1,1 | 1,2 |
 # | 2,0 | 2,1 | 2,2 |
-rows = 3 
+rows = 3
 columns = 3
 board_state = [[-1 for j in range(columns)] for i in range(rows)]
+
+stuff = 0
+
 
 # prints the board to the screen according to the board state
 def print_board(board_state):
     for i in range(rows):
         for j in range(columns):
-            print('|', end = ' ')
-            
-            if board_state[i][j] == -1:
-                print('-', end = ' ')
-            elif board_state[i][j] == 0:
-                print('O', end = ' ')
-            elif board_state[i][j] == 1:
-                print('X', end = ' ')
+            print('|', end=' ')
 
-            print('|', end = ' ')      
+            if board_state[i][j] == -1:
+                print('-', end=' ')
+            elif board_state[i][j] == 0:
+                print('O', end=' ')
+            elif board_state[i][j] == 1:
+                print('X', end=' ')
+
+            print('|', end=' ')
         print()
 
 
@@ -35,7 +38,7 @@ def evaluate_board_state(board_state):
             elif board_state[i][0] == 1:
                 return 10
 
-    # check columing winning conditions
+    # check column winning conditions
     for j in range(columns):
         if board_state[0][j] == board_state[1][j] and board_state[1][j] == board_state[2][j]:
             if board_state[0][j] == 0:
@@ -58,24 +61,25 @@ def evaluate_board_state(board_state):
 
     return 0
 
+
 def moves_left(board_state):
     for i in range(rows):
         for j in range(columns):
             if (board_state[i][j] == -1):
                 return True
-            
+
     return False
 
 
-# minimax algorithm with alpha beta pruning
-def minimax(board_state, depth, is_max, alpha, beta):
+# minimax algorithm
+def minimax(board_state, is_max):
     score = evaluate_board_state(board_state)
-    
+
     if score == 10:
         return score
     elif score == -10:
         return score
-    elif moves_left(board_state) == False:
+    elif not moves_left(board_state):
         return score
 
     if is_max:
@@ -86,7 +90,82 @@ def minimax(board_state, depth, is_max, alpha, beta):
                 if board_state[i][j] == -1:
                     board_state[i][j] = 1
 
-                    best = max(best, minimax(board_state, depth + 1, not is_max, alpha, beta) - depth)
+                    best = max(best, minimax(board_state, not is_max))
+
+                    board_state[i][j] = -1
+        return best
+
+    elif not is_max:
+        best = 1000
+
+        for i in range(rows):
+            for j in range(columns):
+                if board_state[i][j] == -1:
+                    board_state[i][j] = 0
+
+                    best = min(best, minimax(board_state, not is_max))
+
+                    board_state[i][j] = -1
+        return best
+
+
+# minimax algorithm with depth control
+def minimax_depth_control(board_state, depth, is_max):
+    score = evaluate_board_state(board_state)
+
+    if score == 10:
+        return score
+    elif score == -10:
+        return score
+    elif not moves_left(board_state):
+        return score
+
+    if is_max:
+        best = -1000
+
+        for i in range(rows):
+            for j in range(columns):
+                if board_state[i][j] == -1:
+                    board_state[i][j] = 1
+
+                    best = max(best, minimax_depth_control(board_state, depth + 1, not is_max) - depth)
+
+                    board_state[i][j] = -1
+        return best
+
+    elif not is_max:
+        best = 1000
+
+        for i in range(rows):
+            for j in range(columns):
+                if board_state[i][j] == -1:
+                    board_state[i][j] = 0
+
+                    best = min(best, minimax_depth_control(board_state, depth + 1, not is_max) + depth)
+
+                    board_state[i][j] = -1
+        return best
+
+# minimax algorithm with alpha beta pruning and depth control
+def minimax_alpha_beta(board_state, depth, is_max, alpha, beta):
+    score = evaluate_board_state(board_state)
+
+    if score == 10:
+        return score
+    elif score == -10:
+        return score
+    elif not moves_left(board_state):
+        return score
+
+    if is_max:
+        best = -1000
+
+        for i in range(rows):
+            for j in range(columns):
+                if board_state[i][j] == -1:
+                    board_state[i][j] = 1
+
+                    best = max(best, minimax_alpha_beta(board_state, depth + 1, not is_max, alpha, beta) - depth)
 
                     board_state[i][j] = -1
 
@@ -103,7 +182,7 @@ def minimax(board_state, depth, is_max, alpha, beta):
                 if board_state[i][j] == -1:
                     board_state[i][j] = 0
 
-                    best = min(best, minimax(board_state, depth + 1, not is_max, alpha, beta) + depth)
+                    best = min(best, minimax_alpha_beta(board_state, depth + 1, not is_max, alpha, beta) + depth)
 
                     board_state[i][j] = -1
 
@@ -112,17 +191,28 @@ def minimax(board_state, depth, is_max, alpha, beta):
                         break
         return best
 
-def findOpponentBestMove(board_state):
+
+def findOpponentBestMove(board_state, minimax_alg):
     best = 1000
     row = -1
     column = -1
 
+    print('name of alg: ' + minimax_alg.__name__)
+
+    alg = minimax_alg.__name__
     for i in range(rows):
         for j in range(columns):
             if (board_state[i][j] == -1):
                 board_state[i][j] = 0
 
-                move = minimax(board_state, 0, True, -10000, 10000)
+                if alg is 'minimax_alpha_beta':
+                    move = minimax_alg(board_state, 0, True, -10000, 10000)
+                elif alg is 'minimax':
+                    move = minimax_alg(board_state, True)
+                elif alg is 'minimax_depth_control':
+                    move = minimax_alg(board_state, 0, True)
+                else:
+                    print('algorithm not found')
 
                 board_state[i][j] = -1
 
@@ -133,9 +223,11 @@ def findOpponentBestMove(board_state):
 
     return row, column
 
-def o_turn(board_state):
-    row, column = findOpponentBestMove(board_state)
+
+def o_turn(board_state, minimax_alg):
+    row, column = findOpponentBestMove(board_state, minimax_alg)
     board_state[row][column] = 0
+
 
 def x_turn(board_state):
     row, column = input('X: place X at ').split()
@@ -143,21 +235,22 @@ def x_turn(board_state):
     column = int(column)
 
     while (board_state[row][column] != -1):
-        print('board is occupied at row ', row,' column ', column)
+        print('board is occupied at row ', row, ' column ', column)
         row, column = input('X: place X at ').split()
         row = int(row)
         column = int(column)
 
     board_state[row][column] = 1
 
+
 def main():
     print('tic tac toe artificial intelligence\n')
     print_board(board_state)
-    
+
     while evaluate_board_state(board_state) == 0 and moves_left(board_state) == True:
         x_turn(board_state)
         print_board(board_state)
-        o_turn(board_state)
+        o_turn(board_state, minimax_depth_control)
         print_board(board_state)
 
     if (evaluate_board_state(board_state) == 10):
@@ -166,6 +259,7 @@ def main():
         print('ai opponent tic tac toe!')
     else:
         print('draw')
-    
+
+
 if __name__ == "__main__":
     main()
